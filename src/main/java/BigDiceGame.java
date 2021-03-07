7b09782443fffb2eefcc5b736844c3060e130934
Nelis Boucké <nelis@archiwise.com>
2021-03-07 20:49:59 +0100

    r - Extract SomethingTwoPair

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -6,7 +6,7 @@ public class BigDiceGame {
             }
 
             case TwoPair: {
-                return calculateScoreForTwoPair(dice1, dice2, dice3, dice4, dice5);
+                return SomethingTwoPair.calculateScoreForTwoPair(dice1, dice2, dice3, dice4, dice5);
             }
 
             case FullHouse: {
@@ -16,29 +16,6 @@ public class BigDiceGame {
             case FiveOfAKind: {
                 return SomethingFiveOfAKind.somethingFiveOfAKind(dice1, dice2, dice3, dice4, dice5);
             }
-        }
-
-        return 0;
-    }
-
-    private static int calculateScoreForTwoPair(int dice1, int dice2, int dice3, int dice4, int dice5) {
-        var counts1 = new int[6];
-        counts1[dice1 - 1]++;
-        counts1[dice2 - 1]++;
-        counts1[dice3 - 1]++;
-        counts1[dice4 - 1]++;
-        counts1[dice5 - 1]++;
-        var n = 0;
-        var score = 0;
-        for (var i = 0; i < 6; i += 1) {
-            if (counts1[6 - i - 1] >= 2) {
-                n++;
-                score += 6 - i;
-            }
-        }
-
-        if (n == 2) {
-            return score * 2;
         }
 
         return 0;
---
+++ src/main/java/SomethingTwoPair.java
@@ -0,0 +1,24 @@
+public class SomethingTwoPair {
+    static int calculateScoreForTwoPair(int dice1, int dice2, int dice3, int dice4, int dice5) {
+        var counts1 = new int[6];
+        counts1[dice1 - 1]++;
+        counts1[dice2 - 1]++;
+        counts1[dice3 - 1]++;
+        counts1[dice4 - 1]++;
+        counts1[dice5 - 1]++;
+        var n = 0;
+        var score = 0;
+        for (var i = 0; i < 6; i += 1) {
+            if (counts1[6 - i - 1] >= 2) {
+                n++;
+                score += 6 - i;
+            }
+        }
+
+        if (n == 2) {
+            return score * 2;
+        }
+
+        return 0;
+    }
+}
