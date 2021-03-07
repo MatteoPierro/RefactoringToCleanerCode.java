09766efc65d8942e346243237403e09ad0cb6552
Nelis Boucké <nelis@archiwise.com>
2021-03-07 21:18:11 +0100

    r - Introduce local variable

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -2,7 +2,7 @@ public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         {
             if (SomethingPair.is(st)) {
-                return SomethingPair.calculate(dice1, dice2, dice3, dice4, dice5);
+                return new SomethingPair().calculate(dice1, dice2, dice3, dice4, dice5);
             }
         }
         {
