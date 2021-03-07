f644a5e1ffe2c1d58edb3f6616ba80b2d6244c7d
Nelis Boucké <nelis@archiwise.com>
2021-03-07 20:52:54 +0100

    r - Remove redundant else

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -2,11 +2,14 @@ public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         if (st == ScoringType.Pair) {
             return SomethingPair.something(dice1, dice2, dice3, dice4, dice5);
-        } else if (st == ScoringType.TwoPair) {
+        }
+        if (st == ScoringType.TwoPair) {
             return SomethingTwoPair.calculateScoreForTwoPair(dice1, dice2, dice3, dice4, dice5);
-        } else if (st == ScoringType.FullHouse) {
+        }
+        if (st == ScoringType.FullHouse) {
             return SomethingFullHouse.somethingFullHouse(dice1, dice2, dice3, dice4, dice5);
-        } else if (st == ScoringType.FiveOfAKind) {
+        }
+        if (st == ScoringType.FiveOfAKind) {
             return SomethingFiveOfAKind.somethingFiveOfAKind(dice1, dice2, dice3, dice4, dice5);
         }
 
