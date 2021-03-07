26a7bb481cdb70ba05310e62c22f7ed10f1d8148
Nelis Boucké <nelis@archiwise.com>
2021-03-07 20:51:33 +0100

    r - Convert to ifs

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,21 +1,13 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
-        switch (st) {
-            case Pair: {
-                return SomethingPair.something(dice1, dice2, dice3, dice4, dice5);
-            }
-
-            case TwoPair: {
-                return SomethingTwoPair.calculateScoreForTwoPair(dice1, dice2, dice3, dice4, dice5);
-            }
-
-            case FullHouse: {
-                return SomethingFullHouse.somethingFullHouse(dice1, dice2, dice3, dice4, dice5);
-            }
-
-            case FiveOfAKind: {
-                return SomethingFiveOfAKind.somethingFiveOfAKind(dice1, dice2, dice3, dice4, dice5);
-            }
+        if (st == ScoringType.Pair) {
+            return SomethingPair.something(dice1, dice2, dice3, dice4, dice5);
+        } else if (st == ScoringType.TwoPair) {
+            return SomethingTwoPair.calculateScoreForTwoPair(dice1, dice2, dice3, dice4, dice5);
+        } else if (st == ScoringType.FullHouse) {
+            return SomethingFullHouse.somethingFullHouse(dice1, dice2, dice3, dice4, dice5);
+        } else if (st == ScoringType.FiveOfAKind) {
+            return SomethingFiveOfAKind.somethingFiveOfAKind(dice1, dice2, dice3, dice4, dice5);
         }
 
         return 0;
