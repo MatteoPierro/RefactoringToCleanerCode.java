6daa2a35c8cc19fa696253b6de2d711265996854
Nelis Boucké <nelis@archiwise.com>
2021-03-07 20:42:39 +0100

    r - Extract for five of a kind

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -15,19 +15,23 @@ public class BigDiceGame {
             }
 
             case FiveOfAKind: {
-                int[] dices = {dice1, dice2, dice3, dice4, dice5};
-                var counts2 = new int[6];
-                for (int die : dices) {
-                    counts2[die - 1]++;
-                }
+                return somethingFiveOfAKind(dice1, dice2, dice3, dice4, dice5, bigScore);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++) {
-                    if (counts2[i1] == 5) {
-                        return bigScore;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int somethingFiveOfAKind(int dice1, int dice2, int dice3, int dice4, int dice5, int bigScore) {
+        int[] dices = {dice1, dice2, dice3, dice4, dice5};
+        var counts2 = new int[6];
+        for (int die : dices) {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++) {
+            if (counts2[i1] == 5) {
+                return bigScore;
             }
         }
 
