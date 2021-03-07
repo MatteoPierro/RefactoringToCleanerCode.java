efe3609282679e439e204502ecc8de764bfb37ff
Nelis Boucké <nelis@archiwise.com>
2021-03-07 20:40:29 +0100

    r - Extract logic for pair

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -3,21 +3,7 @@ public class BigDiceGame {
         int bigScore = 50;
         switch (st) {
             case Pair: {
-                int value = 2;
-                int[] tallies1;
-                tallies1 = new int[6];
-                tallies1[dice1 - 1]++;
-                tallies1[dice2 - 1]++;
-                tallies1[dice3 - 1]++;
-                tallies1[dice4 - 1]++;
-                tallies1[dice5 - 1]++;
-                for (var i = 6 - 1; i >= 0; i--) {
-                    if (tallies1[i] >= value) {
-                        return (i + 1) * value;
-                    }
-                }
-
-                return 0;
+                return something(dice1, dice2, dice3, dice4, dice5);
             }
 
             case TwoPair: {
@@ -94,6 +80,24 @@ public class BigDiceGame {
                 }
 
                 return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int something(int dice1, int dice2, int dice3, int dice4, int dice5) {
+        int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[dice1 - 1]++;
+        tallies1[dice2 - 1]++;
+        tallies1[dice3 - 1]++;
+        tallies1[dice4 - 1]++;
+        tallies1[dice5 - 1]++;
+        for (var i = 6 - 1; i >= 0; i--) {
+            if (tallies1[i] >= value) {
+                return (i + 1) * value;
             }
         }
 
