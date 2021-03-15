402e9d78baf4bce318c5d9778020969ff60ab404
Nelis Boucké <nelis@archiwise.com>
2021-03-15 20:39:58 +0100

    R!! - Move SomethingFullHouse to many

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,19 +1,14 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         {
-            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair()}) {
+            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair(), new SomethingFullHouse()}) {
                 if (something.is(st)) {
                     return something.calculate(dice1, dice2, dice3, dice4, dice5);
                 }
             }
         }
 
-        {
-            SomethingFullHouse somethingFullHouse = new SomethingFullHouse();
-            if (somethingFullHouse.is(st)) {
-                return somethingFullHouse.calculate(dice1, dice2, dice3, dice4, dice5);
-            }
-        }
+
         {
             SomethingFiveOfAKind somethingFiveOfAKind = new SomethingFiveOfAKind();
             if (somethingFiveOfAKind.is(st)) {
