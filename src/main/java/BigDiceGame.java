5c6e2d7fb9e9a498c027d8d78215d78937399006
Nelis Boucké <nelis@archiwise.com>
2021-03-15 20:42:08 +0100

    R!! - Move SomethingFiveOfAKind to many

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,18 +1,10 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         {
-            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair(), new SomethingFullHouse()}) {
+            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair(), new SomethingFullHouse(), new SomethingFiveOfAKind()}) {
                 if (something.is(st)) {
                     return something.calculate(dice1, dice2, dice3, dice4, dice5);
                 }
-            }
-        }
-
-
-        {
-            SomethingFiveOfAKind somethingFiveOfAKind = new SomethingFiveOfAKind();
-            if (somethingFiveOfAKind.is(st)) {
-                return somethingFiveOfAKind.calculate(dice1, dice2, dice3, dice4, dice5);
             }
         }
 
