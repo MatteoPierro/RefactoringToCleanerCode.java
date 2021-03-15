cd2f2cbdd400a9e86b47d7fd2e61568f051b380c
Nelis Boucké <nelis@archiwise.com>
2021-03-15 20:29:31 +0100

    R!! - many to one for SomethingPair

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,9 +1,10 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         {
-            Something something = new SomethingPair();
-            if (something.is(st)) {
-                return something.calculate(dice1, dice2, dice3, dice4, dice5);
+            for (Something something : new Something[] {new SomethingPair()}) {
+                if (something.is(st)) {
+                    return something.calculate(dice1, dice2, dice3, dice4, dice5);
+                }
             }
         }
         {
