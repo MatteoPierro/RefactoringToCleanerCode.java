8994af1d5f8e6a9c906226d48448e65325e6c475
Nelis Boucké <nelis@archiwise.com>
2021-03-15 20:38:08 +0100

    R!! - Move SomethingTwoPair to many

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,18 +1,13 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         {
-            for (Something something : new Something[] {new SomethingPair()}) {
+            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair()}) {
                 if (something.is(st)) {
                     return something.calculate(dice1, dice2, dice3, dice4, dice5);
                 }
             }
         }
-        {
-            Something something = new SomethingTwoPair();
-            if (something.is(st)) {
-                return something.calculate(dice1, dice2, dice3, dice4, dice5);
-            }
-        }
+
         {
             SomethingFullHouse somethingFullHouse = new SomethingFullHouse();
             if (somethingFullHouse.is(st)) {
