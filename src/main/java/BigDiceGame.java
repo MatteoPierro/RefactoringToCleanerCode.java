96a29ceb1d4cb32fe19e0f5cb9064bca15940b3b
Nelis Boucké <nelis@archiwise.com>
2021-03-15 20:47:21 +0100

    r - convert to stream

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,14 +1,13 @@
+import java.util.stream.Stream;
+
 public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
-        {
-            for (Something something : new Something[] {new SomethingPair(), new SomethingTwoPair(), new SomethingFullHouse(), new SomethingFiveOfAKind()}) {
-                if (something.is(st)) {
-                    return something.calculate(dice1, dice2, dice3, dice4, dice5);
-                }
-            }
-        }
+        return Stream.of(new SomethingPair(), new SomethingTwoPair(), new SomethingFullHouse(), new SomethingFiveOfAKind())
+                .filter(something -> something.is(st))
+                .findFirst()
+                    .map(something -> something.calculate(dice1, dice2, dice3, dice4, dice5))
+                .orElse(0);
 
-        return 0;
     }
 
 }
