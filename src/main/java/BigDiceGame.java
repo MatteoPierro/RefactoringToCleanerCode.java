6377002e96eb00ec58e539a36d3cfcb4ec6ccbd4
Nelis Boucké <nelis@archiwise.com>
2021-03-25 19:54:18 +0100

    r - Rename st to scoringType

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -3,9 +3,9 @@ import java.util.stream.Stream;
 public class BigDiceGame {
     public static final Scorer[] SCORERS = {new ScorerPair(), new ScorerTwoPair(), new ScorerFullHouse(), new ScorerFiveOfAKind()};
 
-    public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
+    public static int CalculateScore(ScoringType scoringType, int dice1, int dice2, int dice3, int dice4, int dice5) {
         return Stream.of(SCORERS)
-                .filter(scorer -> scorer.canCalculate(st))
+                .filter(scorer -> scorer.canCalculate(scoringType))
                 .findFirst()
                     .map(scorer -> scorer.calculate(dice1, dice2, dice3, dice4, dice5))
                 .orElse(0);
