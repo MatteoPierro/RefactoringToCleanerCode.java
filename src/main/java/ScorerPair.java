6e462e81cca42fc97c3a2c8021244e3e4822730c
Nelis Boucké <nelis@archiwise.com>
2021-03-25 20:37:50 +0100

    r - Extract method tally

--- src/main/java/ScorerPair.java
+++ src/main/java/ScorerPair.java
@@ -8,11 +8,7 @@ public class ScorerPair implements Scorer {
     @Override
     public int calculate(Roll roll) {
         int value = 2;
-        int[] tally = new int[6];
-
-        for (int die: roll.dice()) {
-            tally[die - 1]++;
-        }
+        int[] tally = tally(roll);
 
         for (var i = 6 - 1; i >= 0; i--) {
             if (tally[i] >= value) {
@@ -21,6 +17,15 @@ public class ScorerPair implements Scorer {
         }
 
         return 0;
+    }
+
+    private int[] tally(Roll roll) {
+        int[] tally = new int[6];
+
+        for (int die: roll.dice()) {
+            tally[die - 1]++;
+        }
+        return tally;
     }
 
 }
