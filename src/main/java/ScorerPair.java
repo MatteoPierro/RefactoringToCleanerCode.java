1ddda0eac0cd32c10245227b2d17f7ff7c414a0f
Nelis Boucké <nelis@archiwise.com>
2021-03-25 20:21:13 +0100

    r - Rename tallies1 to tally

--- src/main/java/ScorerPair.java
+++ src/main/java/ScorerPair.java
@@ -8,15 +8,15 @@ public class ScorerPair implements Scorer {
     @Override
     public int calculate(Roll roll) {
         int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[roll.getDie1() - 1]++;
-        tallies1[roll.getDie2() - 1]++;
-        tallies1[roll.getDie3() - 1]++;
-        tallies1[roll.getDie4() - 1]++;
-        tallies1[roll.getDie5() - 1]++;
+        int[] tally;
+        tally = new int[6];
+        tally[roll.getDie1() - 1]++;
+        tally[roll.getDie2() - 1]++;
+        tally[roll.getDie3() - 1]++;
+        tally[roll.getDie4() - 1]++;
+        tally[roll.getDie5() - 1]++;
         for (var i = 6 - 1; i >= 0; i--) {
-            if (tallies1[i] >= value) {
+            if (tally[i] >= value) {
                 return (i + 1) * value;
             }
         }
