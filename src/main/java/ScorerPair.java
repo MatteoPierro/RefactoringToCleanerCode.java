4e6eceb7dad32a1bca20318a791845797dfbfb37
Nelis Boucké <nelis@archiwise.com>
2021-03-25 20:28:31 +0100

    R - start many-of-one dice

--- src/main/java/ScorerPair.java
+++ src/main/java/ScorerPair.java
@@ -9,11 +9,15 @@ public class ScorerPair implements Scorer {
     public int calculate(Roll roll) {
         int value = 2;
         int[] tally = new int[6];
-        tally[roll.getDie1() - 1]++;
+
+        for (int die: new int[]{roll.getDie1()}) {
+            tally[die - 1]++;
+        }
         tally[roll.getDie2() - 1]++;
         tally[roll.getDie3() - 1]++;
         tally[roll.getDie4() - 1]++;
         tally[roll.getDie5() - 1]++;
+
         for (var i = 6 - 1; i >= 0; i--) {
             if (tally[i] >= value) {
                 return (i + 1) * value;
