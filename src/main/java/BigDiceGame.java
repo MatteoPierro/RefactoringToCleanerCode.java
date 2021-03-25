6b40c3c77e62f9ec9b6836db87ae8b1fa9b4ef0b
Nelis Boucké <nelis@archiwise.com>
2021-03-25 19:52:35 +0100

    r - Rename to scorer

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -5,9 +5,9 @@ public class BigDiceGame {
 
     public static int CalculateScore(ScoringType st, int dice1, int dice2, int dice3, int dice4, int dice5) {
         return Stream.of(SCORERS)
-                .filter(something -> something.is(st))
+                .filter(scorer -> scorer.is(st))
                 .findFirst()
-                    .map(something -> something.calculate(dice1, dice2, dice3, dice4, dice5))
+                    .map(scorer -> scorer.calculate(dice1, dice2, dice3, dice4, dice5))
                 .orElse(0);
 
     }
