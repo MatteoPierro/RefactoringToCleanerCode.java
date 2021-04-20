2e2466fe90e75b32e4b7fd28dfc8d38ffeb3cc07
Nelis <nelis@boucke.be>
2021-04-20 18:15:24 +0000

    R! - remove var i

--- src/main/java/ScorerFullHouse.java
+++ src/main/java/ScorerFullHouse.java
@@ -12,20 +12,18 @@ public class ScorerFullHouse implements Scorer {
 
     private int calculateScore(int[] tallies) {
         var foundPair = false;
-        int i;
         var _2_at = 0;
         var foundThreeOfAKind = false;
         var _3_at = 0;
 
-
-        for (i = 0; i != 6; i += 1) {
+        for (var i = 0; i != 6; i += 1) {
             if (tallies[i] == 2) {
                 foundPair = true;
                 _2_at = i + 1;
             }
         }
 
-        for (i = 0; i != 6; i += 1) {
+        for (var i = 0; i != 6; i += 1) {
             if (tallies[i] == 3) {
                 foundThreeOfAKind = true;
                 _3_at = i + 1;
