1be5b3d918353ba0ab2d0751aa9821f70a32e9ab
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:13:24 +0300

    r - extract class

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -14,23 +14,7 @@ public class BigDiceGame {
             }
 
             case FiveOfAKind: {
-                return calculateScoreForFiveOfAKind(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-            }
-        }
-
-        return 0;
-    }
-
-    private static int calculateScoreForFiveOfAKind(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
-        int[] dice1 = {firstDie, secondDie, thirdDie, fourthDie, fifthDie};
-        var counts2 = new int[6];
-        for (int die : dice1) {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++) {
-            if (counts2[i1] == 5) {
-                return 50;
+                return FiveOfAKindScoreCalculator.calculateScoreForFiveOfAKind(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
         }
 
---
+++ src/main/java/FiveOfAKindScoreCalculator.java
@@ -0,0 +1,17 @@
+public class FiveOfAKindScoreCalculator {
+    static int calculateScoreForFiveOfAKind(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
+        int[] dice1 = {firstDie, secondDie, thirdDie, fourthDie, fifthDie};
+        var counts2 = new int[6];
+        for (int die : dice1) {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++) {
+            if (counts2[i1] == 5) {
+                return 50;
+            }
+        }
+
+        return 0;
+    }
+}
