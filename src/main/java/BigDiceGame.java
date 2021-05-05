d319b9c90ca56b8beef70e617468d898b34578fd
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:57:52 +0300

    R - many of one

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,24 +1,6 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType scoringType, int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
-        for (ScoreCalculator calculator : new ScoreCalculator[]{ new PairScoreCalculator() }) {
-            if (calculator.isSomething(scoringType)) {
-                return calculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-            }
-        }
-        {
-            ScoreCalculator calculator = new TwoPairScoreCalculator();
-            if (calculator.isSomething(scoringType)) {
-                return calculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-            }
-        }
-        {
-            ScoreCalculator calculator = new FullHouseScoreCalculator();
-            if (calculator.isSomething(scoringType)) {
-                return calculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-            }
-        }
-        {
-            ScoreCalculator calculator = new FiveOfAKindScoreCalculator();
+        for (ScoreCalculator calculator : new ScoreCalculator[]{ new PairScoreCalculator(), new TwoPairScoreCalculator(), new FullHouseScoreCalculator(), new FiveOfAKindScoreCalculator()}) {
             if (calculator.isSomething(scoringType)) {
                 return calculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
