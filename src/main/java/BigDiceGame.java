bae9be8a800ad4de7b9f30b13293463943316303
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:20:00 +0300

    r - extract methods

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -3,17 +3,29 @@ public class BigDiceGame {
         if (isPair(scoringType)) {
             return PairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
-        if (scoringType == ScoringType.TwoPair) {
+        if (isTwoPair(scoringType)) {
             return TwoPairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
-        if (scoringType == ScoringType.FullHouse) {
+        if (isFullHouse(scoringType)) {
             return FullHouseScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
-        if (scoringType == ScoringType.FiveOfAKind) {
+        if (isFiveOfAKind(scoringType)) {
             return FiveOfAKindScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
 
         return 0;
+    }
+
+    private static boolean isFiveOfAKind(ScoringType scoringType) {
+        return scoringType == ScoringType.FiveOfAKind;
+    }
+
+    private static boolean isFullHouse(ScoringType scoringType) {
+        return scoringType == ScoringType.FullHouse;
+    }
+
+    private static boolean isTwoPair(ScoringType scoringType) {
+        return scoringType == ScoringType.TwoPair;
     }
 
     private static boolean isPair(ScoringType scoringType) {
