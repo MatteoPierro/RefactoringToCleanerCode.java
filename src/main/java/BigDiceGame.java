9f0bb40f5fe27fd0788f0879789464eaa18734e9
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:01:59 +0300

    r - extract class Foo

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -2,7 +2,7 @@ public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
         switch (st) {
             case Pair: {
-                return calculateScoreForPair(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+                return Foo.calculateScoreForPair(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
 
             case TwoPair: {
@@ -97,21 +97,4 @@ public class BigDiceGame {
         return 0;
     }
 
-    private static int calculateScoreForPair(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
-        int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[firstDie - 1]++;
-        tallies1[secondDie - 1]++;
-        tallies1[thirdDie - 1]++;
-        tallies1[fourthDie - 1]++;
-        tallies1[fifthDie - 1]++;
-        for (var i = 6 - 1; i >= 0; i--) {
-            if (tallies1[i] >= value) {
-                return (i + 1) * value;
-            }
-        }
-
-        return 0;
-    }
 }
---
+++ src/main/java/Foo.java
@@ -0,0 +1,19 @@
+public class Foo {
+    static int calculateScoreForPair(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
+        int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[firstDie - 1]++;
+        tallies1[secondDie - 1]++;
+        tallies1[thirdDie - 1]++;
+        tallies1[fourthDie - 1]++;
+        tallies1[fifthDie - 1]++;
+        for (var i = 6 - 1; i >= 0; i--) {
+            if (tallies1[i] >= value) {
+                return (i + 1) * value;
+            }
+        }
+
+        return 0;
+    }
+}
