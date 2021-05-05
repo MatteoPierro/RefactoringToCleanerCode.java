ea6b76a0857ceecf9e352d85d68b621f0a49a9c5
Pietro Di Bello <pierodibello@gmail.com>
2021-05-05 21:51:23 +0200

    r - extract method

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -14,19 +14,23 @@ public class BigDiceGame {
             }
 
             case FiveOfAKind: {
-                int[] dice1 = {firstDie, secondDie, thirdDie, fourthDie, fifthDie};
-                var counts2 = new int[6];
-                for (int die : dice1) {
-                    counts2[die - 1]++;
-                }
+                return some(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++) {
-                    if (counts2[i1] == 5) {
-                        return 50;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int some(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
+        int[] dice1 = {firstDie, secondDie, thirdDie, fourthDie, fifthDie};
+        var counts2 = new int[6];
+        for (int die : dice1) {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++) {
+            if (counts2[i1] == 5) {
+                return 50;
             }
         }
 
