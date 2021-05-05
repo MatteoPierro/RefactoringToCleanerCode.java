fdeab0deeafacc3948831dba34a6522d1fa18f33
Pietro Di Bello <pierodibello@gmail.com>
2021-05-05 21:45:53 +0200

    r - rename parameter

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,5 +1,5 @@
 public class BigDiceGame {
-    public static int CalculateScore(ScoringType st, int firstDie, int secondDie, int d3, int dice4, int fifthDie) {
+    public static int CalculateScore(ScoringType st, int firstDie, int secondDie, int thirdDie, int dice4, int fifthDie) {
         int bigScore = 50;
         switch (st) {
             case Pair: {
@@ -8,7 +8,7 @@ public class BigDiceGame {
                 tallies1 = new int[6];
                 tallies1[firstDie - 1]++;
                 tallies1[secondDie - 1]++;
-                tallies1[d3 - 1]++;
+                tallies1[thirdDie - 1]++;
                 tallies1[dice4 - 1]++;
                 tallies1[fifthDie - 1]++;
                 for (var i = 6 - 1; i >= 0; i--) {
@@ -24,7 +24,7 @@ public class BigDiceGame {
                 var counts1 = new int[6];
                 counts1[firstDie - 1]++;
                 counts1[secondDie - 1]++;
-                counts1[d3 - 1]++;
+                counts1[thirdDie - 1]++;
                 counts1[dice4 - 1]++;
                 counts1[fifthDie - 1]++;
                 var n = 0;
@@ -55,7 +55,7 @@ public class BigDiceGame {
                 tallies = new int[6];
                 tallies[firstDie - 1] += 1;
                 tallies[secondDie - 1] += 1;
-                tallies[d3 - 1] += 1;
+                tallies[thirdDie - 1] += 1;
                 tallies[dice4 - 1] += 1;
                 tallies[fifthDie - 1] += 1;
 
@@ -81,7 +81,7 @@ public class BigDiceGame {
             }
 
             case FiveOfAKind: {
-                int[] dice1 = {firstDie, secondDie, d3, dice4, fifthDie};
+                int[] dice1 = {firstDie, secondDie, thirdDie, dice4, fifthDie};
                 var counts2 = new int[6];
                 for (int die : dice1) {
                     counts2[die - 1]++;
