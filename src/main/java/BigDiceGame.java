1431db496951b92a0a71dd90782ce943bf5e25b6
Pietro Di Bello <pierodibello@gmail.com>
2021-05-05 21:47:40 +0200

    r - extract method

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -7,26 +7,7 @@ public class BigDiceGame {
             }
 
             case TwoPair: {
-                var counts1 = new int[6];
-                counts1[firstDie - 1]++;
-                counts1[secondDie - 1]++;
-                counts1[thirdDie - 1]++;
-                counts1[fourthDie - 1]++;
-                counts1[fifthDie - 1]++;
-                var n = 0;
-                var score = 0;
-                for (var i = 0; i < 6; i += 1) {
-                    if (counts1[6 - i - 1] >= 2) {
-                        n++;
-                        score += 6 - i;
-                    }
-                }
-
-                if (n == 2) {
-                    return score * 2;
-                }
-
-                return 0;
+                return bar(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
 
             case FullHouse: {
@@ -81,6 +62,29 @@ public class BigDiceGame {
 
                 return 0;
             }
+        }
+
+        return 0;
+    }
+
+    private static int bar(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
+        var counts1 = new int[6];
+        counts1[firstDie - 1]++;
+        counts1[secondDie - 1]++;
+        counts1[thirdDie - 1]++;
+        counts1[fourthDie - 1]++;
+        counts1[fifthDie - 1]++;
+        var n = 0;
+        var score = 0;
+        for (var i = 0; i < 6; i += 1) {
+            if (counts1[6 - i - 1] >= 2) {
+                n++;
+                score += 6 - i;
+            }
+        }
+
+        if (n == 2) {
+            return score * 2;
         }
 
         return 0;
