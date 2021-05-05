b41c05baf179491e8ab8367111aed23a3e32f6e0
Pietro Di Bello <pierodibello@gmail.com>
2021-05-05 21:46:56 +0200

    r - extract method

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -3,21 +3,7 @@ public class BigDiceGame {
         int bigScore = 50;
         switch (st) {
             case Pair: {
-                int value = 2;
-                int[] tallies1;
-                tallies1 = new int[6];
-                tallies1[firstDie - 1]++;
-                tallies1[secondDie - 1]++;
-                tallies1[thirdDie - 1]++;
-                tallies1[fourthDie - 1]++;
-                tallies1[fifthDie - 1]++;
-                for (var i = 6 - 1; i >= 0; i--) {
-                    if (tallies1[i] >= value) {
-                        return (i + 1) * value;
-                    }
-                }
-
-                return 0;
+                return foo(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
 
             case TwoPair: {
@@ -94,6 +80,24 @@ public class BigDiceGame {
                 }
 
                 return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int foo(int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
+        int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[firstDie - 1]++;
+        tallies1[secondDie - 1]++;
+        tallies1[thirdDie - 1]++;
+        tallies1[fourthDie - 1]++;
+        tallies1[fifthDie - 1]++;
+        for (var i = 6 - 1; i >= 0; i--) {
+            if (tallies1[i] >= value) {
+                return (i + 1) * value;
             }
         }
 
