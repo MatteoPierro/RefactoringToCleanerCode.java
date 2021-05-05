0b176dce56638a299f6694bc1d221b9c44227d38
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:18:21 +0300

    r - extract method

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -1,6 +1,6 @@
 public class BigDiceGame {
     public static int CalculateScore(ScoringType scoringType, int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
-        if (scoringType == ScoringType.Pair) {
+        if (isFoo(scoringType)) {
             return PairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
         if (scoringType == ScoringType.TwoPair) {
@@ -14,6 +14,10 @@ public class BigDiceGame {
         }
 
         return 0;
+    }
+
+    private static boolean isFoo(ScoringType scoringType) {
+        return scoringType == ScoringType.Pair;
     }
 
 }
