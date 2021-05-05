38ca860954d10ef0029552cca9d23e7e770d8866
Matteo Pierro <pierromatteo@gmail.com>
2021-05-05 23:16:09 +0300

    r - remove redundant else

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -2,11 +2,14 @@ public class BigDiceGame {
     public static int CalculateScore(ScoringType st, int firstDie, int secondDie, int thirdDie, int fourthDie, int fifthDie) {
         if (st == ScoringType.Pair) {
             return PairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-        } else if (st == ScoringType.TwoPair) {
+        }
+        if (st == ScoringType.TwoPair) {
             return TwoPairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-        } else if (st == ScoringType.FullHouse) {
+        }
+        if (st == ScoringType.FullHouse) {
             return FullHouseScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
-        } else if (st == ScoringType.FiveOfAKind) {
+        }
+        if (st == ScoringType.FiveOfAKind) {
             return FiveOfAKindScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
         }
 
