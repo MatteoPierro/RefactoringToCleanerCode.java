1b5eedb45c1c2086bfe3779011535bd06cdfa91a
Pietro Di Bello <pierodibello@gmail.com>
2021-05-05 22:35:22 +0200

    r - surround ifs with a block

--- src/main/java/BigDiceGame.java
+++ src/main/java/BigDiceGame.java
@@ -5,14 +5,20 @@ public class BigDiceGame {
                 return PairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
             }
         }
-        if (TwoPairScoreCalculator.isSomething(scoringType)) {
-            return TwoPairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+        {
+            if (TwoPairScoreCalculator.isSomething(scoringType)) {
+                return TwoPairScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+            }
         }
-        if (FullHouseScoreCalculator.isSomething(scoringType)) {
-            return FullHouseScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+        {
+            if (FullHouseScoreCalculator.isSomething(scoringType)) {
+                return FullHouseScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+            }
         }
-        if (FiveOfAKindScoreCalculator.isSomething(scoringType)) {
-            return FiveOfAKindScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+        {
+            if (FiveOfAKindScoreCalculator.isSomething(scoringType)) {
+                return FiveOfAKindScoreCalculator.calculateScore(firstDie, secondDie, thirdDie, fourthDie, fifthDie);
+            }
         }
 
         return 0;
